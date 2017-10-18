66d96fb945a5d30641ce00d96ea9b40f817eca74
samejack <sj@toright.com>
2017-10-18 23:40:19 +0800

    set time zone from config interface on MVC framework start

--- src/Activity.php
+++ src/Activity.php
@@ -12,13 +12,24 @@ class Activity
      * @var \point\core\Context
      */
     private $_context;
-    
+
+    /**
+     * @Autowired
+     * @var \point\web\Config_Interface
+     */
+    private $_config;
+
     public function start()
     {
+        if (!is_null($this->_config->getIterator()) && isset($this->_config->getIterator()['system']['timezone'])) {
+            date_default_timezone_set($this->_config->getIterator()['system']['timezone']);
+        }
+
         $eventHandleManager = $this->_context->getBeanByClassName('\point\core\EventHandleManager');
         $eventHandleManager->addExceptionHandler(
             $this->_context->getBeanByClassName('\point\web\Handler_ExceptionViewer')
         );
+
         // get dispatcher and dispatches
         $request = $this->_context->getBeanByClassName('\point\web\Http_Request');
         $response = $this->_context->getBeanByClassName('\point\web\Http_Response');
