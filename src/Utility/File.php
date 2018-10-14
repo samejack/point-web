c20a6047049381f491a2225f6472d67bb1fa69f3
samejack <sj@toright.com>
2018-10-14 23:11:54 +0800

    improve isImage function

--- src/Utility/File.php
+++ src/Utility/File.php
@@ -9,20 +9,46 @@ namespace point\web;
  */
 class Utility_File
 {
+    public static $allowImageType = [
+        'jpg' => true,
+        'jpeg' => true,
+        'gif' => true,
+        'bmp' => true,
+        'png' => true,
+        'tif' => true
+    ];
+
     public static function isImage($filePath, $checkExtension = true)
     {
         if (file_exists($filePath)) {
-            $info = @getimagesize($filePath);
-            if (!$info)  return false;
-
-            if (!$checkExtension) {
-                return true;
-            } else {
-                $ext = image_type_to_extension($info['2']);
-                return preg_match('/^.+\.((jpg|jpeg|gif|bmp|png))$/', $ext) === 1;
+            if ($checkExtension) {
+                $ext = pathinfo($filePath, PATHINFO_EXTENSION);
+                if (!isset(self::$allowImageType[strtolower($ext)])) {
+                    return false;
+                }
             }
-        } else {
-            return false;
+            $info = @getimagesize($filePath);
+            if ($info)  return true;
         }
+        return false;
+    }
+
+    public static function removeDirectory($path)
+    {
+        if (is_dir($path)) {
+            $files = scandir($path);
+            foreach ($files as $file) {
+                if ($file !== '.' && $file !== '..') {
+                    if (is_dir($path . DIRECTORY_SEPARATOR  . $file)) {
+                        self::removeDirectory($path . DIRECTORY_SEPARATOR . $file);
+                    } else {
+                        unlink($path . DIRECTORY_SEPARATOR . $file);
+                    }
+                }
+            }
+            rmdir($path);
+            return true;
+        }
+        return false;
     }
 }
