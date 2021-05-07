d01a73166f048329c86e2b32b75b22e9ff850878
SJ Chou <sj@toright.com>
2021-05-08 00:17:33 +0800

    fix iPhone 6 IOS 12 Samesite=None can not keep cookie issue

--- src/Session/Plugin.php
+++ src/Session/Plugin.php
@@ -72,9 +72,18 @@ class Session_Plugin implements Session_Interface
             );
         } else {
             $path = $this->_config['session']['path'];
+
+            $sameSite = is_null($path) ? null : $path . '; SameSite=' . $this->_config['session']['samesite'];
+
+            // iPhone 如果送出 SameSite=None 會變成 Strict
+            $headers = getallheaders();
+            if ($sameSite === 'None' && strpos($headers['User-Agent'], 'iPhone OS 12') !== false) {
+                $sameSite = null;
+            }
+
             session_set_cookie_params(
                 $this->_config['session']['lifetime'],
-                is_null($path) ? null : $path . '; SameSite=' . $this->_config['session']['samesite'],
+                $sameSite,
                 $this->_config['session']['domain'],
                 $this->_config['session']['secure'],
                 $this->_config['session']['httponly']
