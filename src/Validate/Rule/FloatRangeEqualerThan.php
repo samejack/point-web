726ea5cba1e56717455c80f51bec69421e6cfd41
SJ Chou <sj@toright.com>
2022-03-22 23:51:32 +0800

    add FloatRangeEqualerThan class

---
+++ src/Validate/Rule/FloatRangeEqualerThan.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace point\web;
+
+/**
+ * 驗證數字範圍
+ * 
+ * @author sj
+ */
+class Validate_Rule_FloatRangeEqualerThan extends Validate_Base
+{
+    /**
+     * 驗證數字範圍
+     *
+     * @param mixed   [$args] 驗證參數 [optional]
+     * <pre>
+     *   $args['min'] = 最小值
+     *   $args['max'] = 最大值
+     * </pre>
+     * @param string  [$message]
+     */
+    public function __construct($args = null, $message = '')
+    {
+        parent::__construct($args, $message);
+    }
+
+    public function validate($input, array &$columns)
+    {
+        //check agrs
+        if (!isset($this->_args['min']) || !isset($this->_args['max'])) {
+            return false;
+        }
+
+        $min = $this->_args['min'];
+        $max = $this->_args['max'];
+        return ((float)$input >= $min && (float)$input <= $max);
+    }
+}
